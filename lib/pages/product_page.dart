3ef0f7a5b1ce1f54952e84647297be5f1588a11c
sizwetshabangu2024 <sizwe.tshabangu@dariel.co.za>
2024-06-20 13:31:07 +0200

    Separating concerns

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:shopify_flutter/models/models.dart';
+import 'package:shopify_hydrogen/widgets/product_card_widget.dart';
+import 'package:shopify_hydrogen/widgets/related_products_widget.dart';
 
 class ProductPage extends StatelessWidget {
-  const ProductPage({super.key, required  this.product});
+  const ProductPage({super.key, required this.product});
 
   final Product product;
 
@@ -11,106 +13,33 @@ class ProductPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
-        backgroundColor: Colors.teal,
+        titleTextStyle: const TextStyle(
+          color: Colors.white
+        ),
+        backgroundColor: Colors.black,
       ),
-      body: Padding(
+      body: SingleChildScrollView(
+        child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Card(
-          elevation: 5,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16.0),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Center(
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black26,
-                          blurRadius: 10,
-                          offset: Offset(0, 5),
-                        ),
-                      ],
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(16.0),
-                      child: Image.network(
-                        product.image,
-                        width: 300,
-                        height: 300,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16.0),
-                ListTile(
-                  title: Text(
-                    product.title,
-                    style: const TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  subtitle: Padding(
-                    padding: const EdgeInsets.only(top: 8.0),
-                    child: Text(
-                      product.description.toString(),
-                      style: const TextStyle(
-                        fontSize: 16,
-                        height: 1.5,
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16.0),
-                Text(
-                  product.formattedPrice,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.green,
-                  ),
-                ),
-                const SizedBox(height: 16.0),
-                Center(
-                  child: SizedBox(
-                    width: double.infinity,
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Add to cart functionality
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text('Product added to cart!'),
-                          ),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.teal,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16.0),
-                        ),
-                      ),
-                      child: const Text(
-                        'Add to Cart',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SingleProductCardWidget(product: product),
+            const SizedBox(height: 32.0),
+            const Text(
+              'Related Products',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
+            const SizedBox(height: 16.0),
+            RelatedProductsWidget(productTitle: product.title, productCollections: product.collectionList ?? []),
+          ],
+        )
         ),
       ),
     );
   }
 }
+
